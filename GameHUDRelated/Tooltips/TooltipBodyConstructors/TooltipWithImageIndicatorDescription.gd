5cb394307e4b8f9a033090765915b0a026087175
tumat001 <castromatthieu23@gmail.com>
2021-04-25 13:59:33 +0800

    Undid color feature.. Was ruining tooltip sizes..

--- GameHUDRelated/Tooltips/TooltipBodyConstructors/TooltipWithImageIndicatorDescription.gd
+++ GameHUDRelated/Tooltips/TooltipBodyConstructors/TooltipWithImageIndicatorDescription.gd
@@ -2,18 +2,14 @@ extends MarginContainer
 
 var description : String
 var img_indicator : Resource
-var description_color : Color
 
-func _init(arg_img_indicator : Resource = null, arg_description : String = "",
-		arg_description_color : Color = Color.black):
+func _init(arg_img_indicator : Resource = null, arg_description : String = ""):
 	description = arg_description
 	img_indicator = arg_img_indicator
-	description_color = arg_description_color
 
 func _ready():
 	$ColumnContainer/ImageIndicator.texture = img_indicator
 	$ColumnContainer/MarginContainer/Label.text = description
-	$ColumnContainer/MarginContainer/Label.add_color_override("font_color", description_color)
 
 func get_info_from_self_class(self_class):
 	description = self_class.description
--- GameHUDRelated/Tooltips/TooltipBodyConstructors/TooltipWithTextIndicatorDescription.gd
+++ GameHUDRelated/Tooltips/TooltipBodyConstructors/TooltipWithTextIndicatorDescription.gd
@@ -1,23 +1,15 @@
 extends MarginContainer
 
 var description : String
-var description_color : Color
 var indicator : String
-var indicator_color : Color
 
-func _init(arg_indicator : String = "", arg_description : String = "",
-		arg_indicator_color : Color = Color.black,
-		arg_description_color : Color = Color.black):
+func _init(arg_indicator : String = "", arg_description : String = ""):
 	description = arg_description
 	indicator = arg_indicator
-	description_color = arg_description_color
-	indicator_color = arg_indicator_color
 
 func _ready():
 	$ColumnContainer/Indicator.text = indicator
-	$ColumnContainer/Indicator.add_color_override("font_color", indicator_color)
 	$ColumnContainer/Label.text = description
-	$ColumnContainer/Label.add_color_override("font_color", description_color)
 
 func get_info_from_self_class(self_class):
 	description = self_class.description
